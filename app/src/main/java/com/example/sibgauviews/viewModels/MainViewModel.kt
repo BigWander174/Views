b550f1e030ba4383a086b5f8ce402690caebaf51
BigWander <BigWander@mail.ru>
2023-10-03 12:40:46 +0700

    Change photo fields and fix issue with two-way binding

--- app/src/main/java/com/example/sibgauviews/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/sibgauviews/viewModels/MainViewModel.kt
@@ -7,10 +7,10 @@ import com.example.sibgauviews.model.Photo
 class MainViewModel : ViewModel() {
     public val album: List<Photo> = listOf(
         Photo("9x12", 100.0),
-        Photo("9x12", 100.0),
-        Photo("9x12", 100.0)
+        Photo("10x15", 150.0),
+        Photo("18x24", 300.0)
         )
-    var amount = ObservableField("0")
+    var amount:String = ""
     var selectedCost: Double = 0.0;
 
     fun select(photoId: Int)
